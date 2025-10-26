7946a9a15e67464721f89b7f31d273f53c204f56
MIchele <12999458@itisrossi.vi.it>
2025-10-26 19:33:46 +0100

    first commit

---
+++ cinematrix.c
@@ -0,0 +1,17 @@
+#include "cinematrix.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define max_string 100
+
+typedef struct {
+    char titolo[max_string100]; // array di caratteri per memorizzare stringhe
+    char regista[max_string100];
+    int anno;
+    float durata;
+} Film;
+
+void aggiungiFilm(Film **catalogo, int *numFilm){
+
+}
