c83fa91654a6476184b3408afcbff5043ec04057
MIchele <12999458@itisrossi.vi.it>
2025-10-26 20:13:46 +0100

    second push

--- cinematrix.c
+++ cinematrix.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+#include <stdin.h>
+#include <time.h>
 
 #define max_string 100
 
@@ -12,6 +15,27 @@ typedef struct {
     float durata;
 } Film;
 
-void aggiungiFilm(Film **catalogo, int *numFilm){
+void aggiungiFilm(Film *catalogo, int *numFilm) {
 
+printf("Inserisci titolo: ");
+gets(catalogo->titolo);
+printf("Inserisci regista: ");
+gets(catalogo->regista);
+printf("Inserisci anno: ");
+scanf("%d", &catalogo->anno);
+printf("Inserisci durata: ");
+scanf("%f", &catalogo->durata);
+
+
+
+}
+
+int main(void) {
+
+    Film *catalogo = (Film *)malloc(sizeof(Film));
+    int numFilm = 0;
+    realloc(catalogo, sizeof(Film));
+
+
+    return 0;
 }
